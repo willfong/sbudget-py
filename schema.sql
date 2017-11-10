49af0713ee5196e7985f7a5d3c7fee6689fe159c
Will Fong <w@r10e.xyz>
2017-11-10 09:10:47 +0800

    Add index on username

--- schema.sql
+++ schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE users (
   displayCurrency TEXT,
   exchangeRate NUMERIC
 );
+CREATE UNIQUE INDEX udx_username ON users (username);
 INSERT INTO users (username, password, monthlyBudget, decimalPlaces, displayCurrency, exchangeRate) VALUES ('will', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 1000, 2, 'S$', 1);
 
 
